046bd0ee5ae4010b34840b174d5d308f1eb0fc72
soni kumari <sonykumarinaiya.sk@gmail.com>
2018-08-01 22:35:23 +0530

    code
    program code for system class

---
+++ Addition1.java
@@ -0,0 +1,8 @@
+class Addition1{
+	public static void main(String[]args){
+	int a=Integer.parseInt(System.getProperty("sno"));
+	int b=Integer.parseInt(System.getProperty("fno"));
+	int c=a+b;
+	System.out.println("Result:"+c);
+	}
+}
